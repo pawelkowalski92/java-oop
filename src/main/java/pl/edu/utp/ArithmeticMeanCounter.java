0a68ea65f1004885c6ca9b77d4c6600b9de4b876
Paweł Kowalski <pawel.kow.92@gmail.com>
2021-03-22 17:31:56 +0100

    Calculate arithmetic mean using structural programming

---
+++ src/main/java/pl/edu/utp/ArithmeticMeanCounter.java
@@ -0,0 +1,16 @@
+package pl.edu.utp;
+
+// structural programming
+public class ArithmeticMeanCounter {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = 40;
+
+        int sum = a + b + c;
+        double mean = sum / 3;
+        System.out.println(mean);
+    }
+
+}
