b19eecef4c747598bcb68da49e74e8e0ea0f34d2
Paweł Kowalski <pawel.kow.92@gmail.com>
2021-03-25 18:50:08 +0100

    Explain class structure - example

--- src/main/java/pl/edu/utp/StructureTest.java
+++ src/main/java/pl/edu/utp/StructureTest.java
@@ -4,9 +4,15 @@ public class StructureTest {
 
     int a = 0;
 
+    public StructureTest() {
+        System.out.println("hi from constructor!");
+        a = 10;
+    }
+
     //code block
     {
         System.out.println("hi! I'm code block");
+        a = 5;
     }
 
     void displayA() {
